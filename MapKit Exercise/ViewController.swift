8eb417de5a2345a0c8d49204e1acc6460a153639
Jules Lee <jackconsolidated@gmail.com>
2019-07-31 03:01:24 +0800

    Initial Commit

---
+++ MapKit Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKit Exercise
+//
+//  Created by Jules Lee on 31/07/2019.
+//  Copyright © 2019 Jules Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
